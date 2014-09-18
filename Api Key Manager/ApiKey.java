e27fd2cff7a17e05e75514f9ba5a389e8725efd0
sjones1272 <stephen.jones@vikings.berry.edu>
2014-09-18 16:55:37 -0400

    ApiKey class

---
+++ Api Key Manager/ApiKey.java
@@ -0,0 +1,12 @@
+public class ApiKey {
+
+ String name, method, key;
+ 
+ //name represents the description, method the call, and key the value of it.
+ ApiKey(String name, String method, String key){
+  this.name = name;
+  this.method = method;
+  this.key = key;
+ }
+ 
+}
